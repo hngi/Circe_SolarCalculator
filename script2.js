4d71dc09960305c30c07ed8ca16ab88562a3c9d4
EricoMartin <martinirex@yahoo.co.uk>
2019-09-25 18:18:27 +0100

    commit for faq.html page

---
+++ script2.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.acc h4').click(function(){ 
+    $(this).next('.ans').slideToggle(); 
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().children('.ans').slideUp(); 
+    $(this).parent().siblings().removeClass('active'); 
+    });
+});
+    
